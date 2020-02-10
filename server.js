9ca1e4433e2b0771d191003f1e80f8fee17eb616
Sarah <sarahd.rios@gmail.com>
2020-02-09 20:16:21 -0600

    test

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 
 
-app.get('/', (req, res) => res.render('home.ejs', {page:'home'}));
-app.get('/projects', (req, res) => res.render('projects.ejs', {page:'projects'}));
-app.get('/skills', (req, res) => res.render('skills.ejs', {page:'skills'}));
-app.get('/resume', (req, res) => res.render('resume.ejs', {page:'resume'}));
+app.get('/', (req, res) => res.render('home.ejs', {page:'about'}));
+app.get('/projects', (req, res) => res.render('home.ejs', {page:'projects'}));
+app.get('/skills', (req, res) => res.render('home.ejs', {page:'skills'}));
+app.get('/resume', (req, res) => res.render('home.ejs', {page:'resume'}));
 
 app.listen(port, () => console.log(`Portfolio listening on port ${port}!`));
